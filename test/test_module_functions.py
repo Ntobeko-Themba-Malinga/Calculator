fd16c0c5569e0c12586c87f087a495e0c64d9488
Ntobeko_Malinga <ntobekothe2@gmail.com>
2022-06-07 16:00:15 +0200

    functions test cases refactored code

--- test/test_module_functions.py
+++ test/test_module_functions.py
@@ -2,65 +2,37 @@ import unittest
 from calculator import main
 
 
+def button_press_simulated(*args):
+    for value in args:
+        try:
+            main.button_press(value)
+        except AttributeError:
+            pass
+
+
 class TestModuleFunctions(unittest.TestCase):
 
     def tearDown(self):
         main.equation_calculation = ''
         main.equation_text = ''
 
-    def test_button_press_1(self):
-        try:
-            main.button_press('2')
-        except AttributeError:
-            pass
-
-        try:
-            main.button_press('**')
-        except AttributeError:
-            pass
-
-        try:
-            main.button_press('3')
-        except AttributeError:
-            pass
+    def test_function_button_press_1(self):
+        """Testing the button_press function"""
+        button_press_simulated('2', '**', '3')
 
         self.assertEqual(main.equation_text, '2^3')
         self.assertEqual(main.equation_calculation, '2**3')
 
-    def test_button_press_2(self):
-        try:
-            main.button_press('2')
-        except AttributeError:
-            pass
-
-        try:
-            main.button_press('*')
-        except AttributeError:
-            pass
-
-        try:
-            main.button_press('3')
-        except AttributeError:
-            pass
+    def test_function_button_press_2(self):
+        """Testing the button_press function"""
+        button_press_simulated('2', '*', '3')
 
         self.assertEqual(main.equation_text, '2x3')
         self.assertEqual(main.equation_calculation, '2*3')
 
-    def test_button_clear(self):
-        try:
-            main.button_press('2')
-        except AttributeError:
-            pass
-
-        try:
-            main.button_press('*')
-        except AttributeError:
-            pass
-
-        try:
-            main.button_press('3')
-        except AttributeError:
-            pass
+    def test_function_clear(self):
+        """Testing the clear function"""
+        button_press_simulated('2', '*', '3')
 
         try:
             main.clear()
@@ -69,21 +41,9 @@ class TestModuleFunctions(unittest.TestCase):
         self.assertEqual(main.equation_text, '')
         self.assertEqual(main.equation_calculation, '')
 
-    def test_function_equal(self):
-        try:
-            main.button_press('2')
-        except AttributeError:
-            pass
-
-        try:
-            main.button_press('*')
-        except AttributeError:
-            pass
-
-        try:
-            main.button_press('3')
-        except AttributeError:
-            pass
+    def test_function_equal_1(self):
+        """Testing the equal function if it returns the correct expected output"""
+        button_press_simulated('2', '*', '3')
 
         try:
             main.equal()
@@ -91,21 +51,29 @@ class TestModuleFunctions(unittest.TestCase):
             pass
         self.assertEqual(main.equation_calculation, '6')
 
+    def test_function_equal_2(self):
+        """Testing the equal function when there's a syntax error in the input"""
+        button_press_simulated('/', '*', '+')
+
+        try:
+            main.equal()
+        except AttributeError:
+            pass
+        self.assertEqual(main.equation_calculation, '/*+')
+
+    def test_function_equal_3(self):
+        """Testing the equal function when the input includes dividing by 0"""
+        button_press_simulated('3', '/', '0')
+
+        try:
+            main.equal()
+        except AttributeError:
+            pass
+        self.assertEqual(main.equation_calculation, '3/0')
+
     def test_function_backspace(self):
-        try:
-            main.button_press('2')
-        except AttributeError:
-            pass
-
-        try:
-            main.button_press('**')
-        except AttributeError:
-            pass
-
-        try:
-            main.button_press('3')
-        except AttributeError:
-            pass
+        """Testing the backspace function"""
+        button_press_simulated('2', '**', '3')
 
         try:
             main.backspace()
